5aceba06e11e6471663d3a8cd7e65044287eb65c
Rahul Biswas <49182089+rahulbiswas12@users.noreply.github.com>
2024-02-09 15:42:33 +0530

    Update csv_diff.py

--- csv_diff.py
+++ csv_diff.py
@@ -3,7 +3,12 @@ import pandas as pd
 try:
     df1 = pd.read_csv('df1.csv')
     df2 = pd.read_csv('df2.csv')
-
+'''
+# Method1 : isin function/method
+    diff = df1[~df1.apply(tuple, 1).isin(df2.apply(tuple, 1))]
+    print(diff)
+'''
+# Method2: Merge
     diff2 = df1.merge(df2,indicator=True, how ='outer').loc[lambda v: v['_merge'] != 'both']
 
     # Drop the '_merge' column
